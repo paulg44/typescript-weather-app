7a011753d8b1b10f4ce1961d890ebe52a6b489da
paulg44 <paulgarton1984@hotmail.com>
2023-08-19 21:42:04 +0100

    start creating cards for store

--- my-app/src/ShoppingCart/ShopingPages/ShoppingStore.tsx
+++ my-app/src/ShoppingCart/ShopingPages/ShoppingStore.tsx
@@ -1,10 +1,20 @@
 // Commponet for shopping cart store page
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../ShoppingData/items.json";
+import StoreItem from "../ShoppingComponents/StoreItem";
 
 function ShoppingStore() {
   return (
-    <div>
-      <h1>Store</h1>
-    </div>
+    <>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {/* Maps through store item json file */}
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
 
