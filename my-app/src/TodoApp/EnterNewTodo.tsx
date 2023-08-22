611b2d48ad365e6ecc242df20f9cd0aea817196d
paulg44 <paulgarton1984@hotmail.com>
2023-08-22 20:32:21 +0100

    start use states and functionality for add new todo to list

--- my-app/src/TodoApp/EnterNewTodo.tsx
+++ my-app/src/TodoApp/EnterNewTodo.tsx
@@ -1,18 +1,33 @@
 // Component for entering new todo
 import { Container, Form, Button } from "react-bootstrap";
+import { EnterNewTodoProps } from "./TodoDisplay";
+// import { ChangeEvent, useState } from "react";
 
-function EnterNewTodo() {
+function EnterNewTodo({
+  handleAddTodo,
+  handleTodoInputDate,
+  handleTodoInputChange,
+}: EnterNewTodoProps) {
   return (
     <Container className=" d-flex align-items-center justify-content-center">
       <Form.Label>Enter new item</Form.Label>
-      <Form.Control className="m-3" style={{ width: "400px" }}></Form.Control>
+      <Form.Control
+        className="m-3"
+        style={{ width: "400px" }}
+        onChange={handleTodoInputChange}
+      ></Form.Control>
       <Form.Label>Due By</Form.Label>
-      <Form.Control className="m-3" style={{ width: "200px" }}></Form.Control>
+      <Form.Control
+        className="m-3"
+        style={{ width: "200px" }}
+        onChange={handleTodoInputDate}
+      ></Form.Control>
 
       <Button
         variant="outline-primary"
         className="shadow"
         style={{ borderRadius: "20px", width: "100px" }}
+        onClick={handleAddTodo}
       >
         Add
       </Button>
--- my-app/src/TodoApp/TodoDisplay.tsx
+++ my-app/src/TodoApp/TodoDisplay.tsx
@@ -1,14 +1,54 @@
 // Component for todo display
 import { Container } from "react-bootstrap";
+import { useState, ChangeEvent, MouseEventHandler } from "react";
 import EnterNewTodo from "./EnterNewTodo";
 import TodoList from "./TodoList";
 
+export type EnterNewTodoProps = {
+  handleTodoInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleTodoInputDate: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleAddTodo: (e: MouseEventHandler<HTMLButtonElement>) => void;
+  input: string;
+  date: string;
+  addTodo: string;
+  newTodo: string;
+};
+
 function TodoDisplay() {
+  // States
+  const [input, setInput] = useState("");
+  const [date, setDate] = useState("");
+  const [addTodo, setAddTodo] = useState<EnterNewTodoProps[]>([]);
+
+  // Function for todo change
+  function handleTodoInputChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setInput(e.target.value);
+    console.log(input);
+  }
+
+  //   Function for todo date
+  function handleTodoInputDate(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setDate(e.target.value);
+    console.log(date);
+  }
+
+  //   Function to add new todod to list
+  function handleAddTodo() {
+    const newTodo = { input, date };
+    setAddTodo([...addTodo, newTodo]);
+    console.log("add todo btn clicked");
+  }
   return (
     <Container className="d-flex flex-column align-items-center">
       {/* Make Paul's dynamic? */}
       <h1>Paul's Todo List</h1>
-      <EnterNewTodo />
+      <EnterNewTodo
+        handleAddTodo={handleAddTodo}
+        handleTodoInputChange={handleTodoInputChange}
+        handleTodoInputDate={handleTodoInputDate}
+      />
       <TodoList />
     </Container>
   );
