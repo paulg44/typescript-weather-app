79d3b6e544db2b3ab51f96585d0ca6c221f012b8
paulg44 <paulgarton1984@hotmail.com>
2023-08-23 21:08:54 +0100

    basic homepage styling complete

--- my-app/src/Homepage/Homepage.tsx
+++ my-app/src/Homepage/Homepage.tsx
@@ -1,29 +1,52 @@
 // Component for Homepage
 import { Link } from "react-router-dom";
-import { Container, Card } from "react-bootstrap";
+import { Container, Stack, Card } from "react-bootstrap";
+import "./Homepage.css";
 
 function Homepage() {
   return (
     <Container
-      className="d-flex  align-items-center justify-content-center"
+      className="d-flex flex-column  align-items-center justify-content-center"
       style={{ height: "100vh" }}
     >
-      <Card>
-        <Card.Title>Weather App</Card.Title>
-        <Link to={"/weather"}>Link</Link>
-      </Card>
-      <Card>
-        <Card.Title>Hangman</Card.Title>
-        <Link to={"/hangman"}>Link</Link>
-      </Card>
-      <Card>
-        <Card.Title>Shopping Cart</Card.Title>
-        <Link to={"/shopping"}>Link</Link>
-      </Card>
-      <Card>
-        <Card.Title>Todo App</Card.Title>
-        <Link to={"/todo"}>Link</Link>
-      </Card>
+      <h1 className="mb-5">
+        Typescript Projects
+        <small className="text-muted"> Styling with Bootstrap</small>
+      </h1>
+      <Stack
+        direction="horizontal"
+        gap={3}
+        className="d-flex align-items-center justify-content-center"
+      >
+        <Card className="customCard">
+          <Card.Title>Weather App</Card.Title>
+          <Card.Body>Some Text</Card.Body>
+          <Link className="link" to={"/weather"}>
+            Link
+          </Link>
+        </Card>
+        <Card className="customCard">
+          <Card.Title>Hangman</Card.Title>
+          <Card.Body>Some Text</Card.Body>
+          <Link className="link" to={"/hangman"}>
+            Link
+          </Link>
+        </Card>
+        <Card className="customCard">
+          <Card.Title>Shopping Cart</Card.Title>
+          <Card.Body>Some Text</Card.Body>
+          <Link className="link" to={"/shopping"}>
+            Link
+          </Link>
+        </Card>
+        <Card className="customCard">
+          <Card.Title>Todo App</Card.Title>
+          <Card.Body>Some Text</Card.Body>
+          <Link className="link" to={"/todo"}>
+            Link
+          </Link>
+        </Card>
+      </Stack>
     </Container>
   );
 }
